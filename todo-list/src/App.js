f3aed8966d2851b59e7d49493fd5dc51b37a9cf8
이진석 <sonaky47@gmail.com>
2017-08-26 23:08:34 +0900

    todo-list 복습중

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import shortId from 'shortid';
 import './App.css';
+import TodoInsert from './components/TodoInsert';
+import TodoItemList from './components/TodoItemList';
+
+function createItem (name) {
+  return {
+    id: shortId.generate(),
+    name,
+    finished: false
+  }
+}
+
+const defaultTodos = [
+  '사업자 등록',
+  'You don\'t know JS'
+].map(createItem);
 
 class App extends Component {
+  state = {
+    todoItems: defaultTodos
+  };
+
+  handleInsert = (name) => {
+    this.setState({
+      todoItems: [
+        ...this.state.todoItems, createItem(name)
+      ]
+    });
+  };
+
+  handleToggle = (id) => {
+    const { todoItems } = this.state;
+    const index = todoItems.findIndex(item => (item.id === id));
+    const item = todoItems[index];
+    this.setState({
+      todoItems: [
+        ...todoItems.slice(0, index),
+        {
+          ...item,
+          finished: !item.finished
+        },
+        ...todoItems.slice(index + 1, todoItems.length)
+      ]
+    });
+  };
+
+  handleRemove = (id) => {
+    const { todoItems } = this.state;
+    const index = todoItems.findIndex(item => (item.id === id));
+    this.setState({
+      todoItems: [
+        ...todoItems.slice(0, index),
+        ...todoItems.slice(index + 1, todoItems.length)
+      ]
+    });
+  }
+
+  handleReset = () => {
+    this.setState({
+      todoItems: []
+    })
+  };
+
   render() {
+    const { todoItems } = this.state;
+    const {
+      handleInsert,
+      handleToggle,
+      handleRemove,
+      handleReset
+    } = this;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>내가 할 일</h2>
+        <TodoInsert onInsert={handleInsert}/>
+        <TodoItemList
+          items={todoItems}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+          />
       </div>
     );
   }
