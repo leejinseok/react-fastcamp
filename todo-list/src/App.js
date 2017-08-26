db4d3fe8e91bc85fb411ef947bfe8f9cbab21b3e
이진석 <sonaky47@gmail.com>
2017-08-26 20:55:27 +0900

    Remove

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,103 +1,18 @@
 import React, { Component } from 'react';
-import shortId from 'shortid'
-import TodoInsert from './components/TodoInsert';
-import TodoItemList from './components/TodoItemList';
-
-function createItem (name) {
-   return {
-        id: shortId.generate(),
-        name,
-        finished: false
-    }
-}
-
-// 기본 Todo 목록 설정
-const defaultTodos = [
-    '리액트 시작하기',
-    '컴포넌트 이해하기',
-    'props/state 사용하기',
-    'LifeCycle API'
-].map(createItem); // 각 원소들을 createItem 함수를 실행해서 변환해준다
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  state = {
-    todoItems: defaultTodos
-  }
-
-  constructor (props) {
-    super(props);
-    const todos = localStorage.getItem('todos');
-    if (todos) {
-      this.state = {
-        todoItems: JSON.parse(todos)
-      }
-    }
-  }
-
-  componentDidUpdate (prevProps, prevState) {
-    localStorage.setItem('todos', JSON.stringify(this.state.todoItems));
-  }
-
-  handleInsert = (name) => {
-    this.setState({
-      todoItems: [
-        ...this.state.todoItems, createItem(name)
-      ]
-    });
-  }
-
-  handleToggle = (id) => {
-    const { todoItems } = this.state;
-    const index = todoItems.findIndex((item) => item.id === id);
-    const item = todoItems[index];
-
-    this.setState({
-      todoItems: [
-        ...todoItems.slice(0, index),
-        {
-          ...item,
-          finished: !item.finished
-        },
-        ...todoItems.slice(index + 1, todoItems.length)
-      ]
-    })
-  }
-
-  handleRemove = (id) => {
-    const { todoItems } = this.state;
-    const index = todoItems.findIndex((item) => item.id === id);
-
-    this.setState({
-      todoItems: [
-        ...todoItems.slice(0, index),
-        ...todoItems.slice(index + 1, todoItems.length)
-      ]
-    })
-  }
-
-  handleReset = () => {
-    this.setState({
-      todoItems: defaultTodos
-    })
-  }
-
   render() {
-    const {
-      handleInsert,
-      handleToggle,
-      handleRemove,
-      handleReset
-    } = this;
-
-    const { todoItems } = this.state;
     return (
       <div className="App">
-        <h1>TODO LIST</h1>
-        <TodoInsert onInsert={handleInsert}/>
-        <TodoItemList
-          items={todoItems}
-          onToggle={handleToggle}
-          onRemove={handleRemove}/>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
