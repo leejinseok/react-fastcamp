bd57851021d7a5210a75a8b05c51363137d3a733
이진석 <sonaky47@gmail.com>
2017-08-29 00:50:25 +0900

    주소록 review 작업중

--- contact-review/src/App.js
+++ contact-review/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import Container from './components/Container';
 
 class App extends Component {
-  render() {
-    return (
+  render(){
+    return(
       <div>
         <Header />
-        <Container visible/>
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
