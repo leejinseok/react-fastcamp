78450cb569fab42690d18a47e3e82726f8487796
이진석 <sonaky47@gmail.com>
2017-08-29 14:59:49 +0900

    컨테이너 생성

--- contact-review/src/App.js
+++ contact-review/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Container from './components/Container';
 
 class App extends Component {
   render(){
     return(
       <div>
         <Header />
+        <Container />
       </div>
     )
   }
