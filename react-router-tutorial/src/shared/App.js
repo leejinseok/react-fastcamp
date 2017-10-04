c2d7b2defc76b007596a2026654668e505b80039
dooly <sonaky47@gmail.com>
2017-10-04 12:50:43 +0900

    Route 기본 라우팅 (/, /about)

--- react-router-tutorial/src/shared/App.js
+++ react-router-tutorial/src/shared/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Home, About } from 'pages';
 
 class App extends Component {
 
   render() {
     return (
         <div>
-            Hello React-Router
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About}/>
         </div>
     );
   }
