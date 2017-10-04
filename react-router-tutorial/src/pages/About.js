ec3f72078f7511811800ecfef158c4d828a6375f
dooly <sonaky47@gmail.com>
2017-10-04 13:07:54 +0900

    query-string을 사용한 조건부 렌더링

--- react-router-tutorial/src/pages/About.js
+++ react-router-tutorial/src/pages/About.js
@@ -3,13 +3,15 @@ import queryString from 'query-string';
 
 const About = ({location, match}) => {
     const query = queryString.parse(location.search);
-    console.log(query);
-    
+
+    const detail = query.detail === 'true';
+
     return(
         <div>
             <h2>
                 About {match.params.name}
             </h2>
+            { detail && 'detail: blahblahblah' }
         </div>
     )
 };
