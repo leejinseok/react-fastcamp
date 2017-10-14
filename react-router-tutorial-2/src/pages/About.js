99a00b5a47dd9bf2617d2760ed8d6e884c3b4d67
dooly <sonaky47@gmail.com>
2017-10-14 15:02:27 +0900

    react rounter 초기 셋

---
+++ react-router-tutorial-2/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import queryString from 'query-string';
+
+const About = ({match, location}) => {
+    const query = queryString.parse(location.search);
+    const detail = query.detail === 'true';
+    return (
+        <div>
+            <h2>{match.params.name} 소개</h2>
+            {detail && 'detail: blahblah'}
+        </div>
+    );
+};
+export default About;
