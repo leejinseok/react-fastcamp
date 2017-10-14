b93cf4bb5e49b8446f0529ec2670852e481e6a49
dooly <sonaky47@gmail.com>
2017-10-14 18:02:45 +0900

    koa, koa-router

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,19 +1,32 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-    console.log('1');
-    next();
+router.get('/', (ctx, next) => {
+    ctx.body = '홈';
 });
 
-app.use((ctx, next) => {
-    console.log('2');
-    next();
+router.get('/about', (ctx, next) => {
+    ctx.body = '소개';
 });
 
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
+router.get('/about/:name', (ctx, next) => {
+    const { name } = ctx.params;
+    ctx.body = `${name}` + '소개';
 });
+
+router.get('/post', (ctx, next) => {
+    const { id } = ctx.request.query; // 주소 뒤에 ?id=10 이런식으로 작성된 쿼리는 ctx.request.query 에 파싱됩니다.
+    if (id) {
+        ctx.body = '포스트 #' + id;
+    } else {
+        ctx.body = '포스트 아이디가 없습니다.';
+    }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('heurm server is listening to port 4000');
